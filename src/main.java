a32b11091c8906d5a143246bddc5c40660c661f4
brunocapim <loginobsequio.com>
2022-05-29 14:56:52 -0300

    final commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        Conta cc = new ContaCorrente();
+        cc.depositar(100);
+        Conta cp = new ContaPoupanca();
+
+        cc.transferir(100, cp);
+
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
+    }
+}
